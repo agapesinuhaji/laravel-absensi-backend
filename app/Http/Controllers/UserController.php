8fd3fe2740ee5d692bf8e40d047852039d6e508d
agapesinuhaji <agapemanase27@gmail.com>
2024-06-03 15:04:19 +0700

    user management

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::where('name', 'like', '%' . request('name') . '%')
+        ->orderBy('id', 'desc')
+        ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
+    }
+
+    public function edit(User $user)
+    {
+        return view('pages.users.edit', compact('user'));
+    }
+
+
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+        ]);
+
+        if ($request->passowrd) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
+    }
+
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
+    }
+
+}
